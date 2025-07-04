15710ea1b3678201f83fafe1f74d977d45647194
Débora <deboraalves007@hotmail.com>
2025-07-04 14:19:08 -0300

    Update grafo

--- Dijkstra/main.py
+++ Dijkstra/main.py
@@ -3,18 +3,22 @@ import networkx as nx
 from copy import deepcopy
 import matplotlib.pyplot as plt
 
+# Grafo corrigido (FL26 substituído por Verdes Mares)
 mapa = {
-    'Bela Vista': {'Uepa': 5},
-    'Uepa': {'FL33': 7, 'Bambuzal': 5, 'Bela Vista': 5},
-    'FL33': {'Uepa': 7, 'C1': 6, 'C2': 10, 'C3': 16, 'Bambuzal': 6, 'FL26': 4, 'São Félix': 19},
-    'C1': {'C3': 13, 'C2': 6, 'São Félix': 15, 'FL26': 3, 'FL33': 6},
-    'C2': {'FL33': 10, 'C1': 6, 'São Félix': 10, 'FL26': 7, 'Transmangueira': 10},
-    'Bambuzal': {'FL33': 6, 'Uepa': 10, 'FL26': 4, 'Transmangueira': 4},
-    'Transmangueira': {'FL26': 9, 'C2': 11, 'Bambuzal': 8},
-    'FL26': {'FL33': 4, 'C2': 9, 'Transmangueira': 9, 'Bambuzal': 6, 'C1': 3},
-    'C3': {'C2': 17, 'C1': 13, 'FL33': 16},
-    'São Félix': {'C1': 15, 'C2': 10, 'FL26': 17, 'FL33': 19, 'Morada Nova': 15},
-    'Morada Nova': {'São Félix': 15}
+    'Bela Vista': {'Uepa': 5, 'Cidade Nova': 6},
+    'Uepa': {'FL33': 7, 'Bambuzal': 5, 'Bela Vista': 5, 'Cidade Nova': 4},
+    'FL33': {'Uepa': 7, 'C1': 6, 'C2': 10, 'C3': 16, 'Bambuzal': 6, 'Verdes Mares': 4, 'São Félix': 19, 'Cidade Nova': 9, 'Shopping': 5},
+    'C1': {'C3': 13, 'C2': 6, 'São Félix': 15, 'Verdes Mares': 3, 'FL33': 6, 'Shopping': 6},
+    'C2': {'FL33': 10, 'C1': 6, 'São Félix': 10, 'Verdes Mares': 7, 'Transmangueira': 10},
+    'Bambuzal': {'FL33': 6, 'Uepa': 10, 'Verdes Mares': 4, 'Transmangueira': 4},
+    'Transmangueira': {'Verdes Mares': 9, 'C2': 11, 'Bambuzal': 8},
+    'Verdes Mares': {'FL33': 4, 'C2': 9, 'Transmangueira': 9, 'Bambuzal': 6, 'C1': 3},
+    'C3': {'C2': 17, 'C1': 13, 'FL33': 16, 'Cidade Jardim': 10},
+    'São Félix': {'C1': 15, 'C2': 10, 'Verdes Mares': 17, 'FL33': 19, 'Morada Nova': 15},
+    'Morada Nova': {'São Félix': 15},
+    'Cidade Jardim': {'C3': 10, 'Shopping': 7},
+    'Shopping': {'Cidade Jardim': 7, 'C1': 6, 'FL33': 5},
+    'Cidade Nova': {'Bela Vista': 6, 'Uepa': 4, 'FL33': 9}
 }
 
 def dijkstra(grafo, inicio, fim):
@@ -35,7 +39,6 @@ def dijkstra(grafo, inicio, fim):
 
 def rota_total(grafo):
     origem = 'Transmangueira'
-    destino = 'C2'
     restantes = list(grafo.keys())
     rota = [origem]
     total = 0
